e71d0984b2ad725fece9a787f6f8ea66a6caae82
mecaota <mecaota@users.noreply.github.com>
2018-04-10 22:31:56 +0900

    delete js's dialog code, and add slack token request code

--- js/slack-fetch.js
+++ js/slack-fetch.js
@@ -1,16 +1,20 @@
-/*  ダイアログ表示 */
-var dialog = document.querySelector('dialog');
-var showDialog = document.body;
-console.log(showDialog);
-if (! dialog.showModal) {
-    dialogPolyfill.registerDialog(dialog);
+var args = getURLparam(location.search.substring(1).split('&'));
+var token = getAccessToken();
+
+
+function getURLparam(pair){
+    var arg = new Object;
+    for(var i = 0;pair[i];i++) {
+        var kv = pair[i].split('=');
+        arg[kv[0]] = kv[1];
+    }
+    return arg;
 }
-console.log("DOMcomplete");
-showDialog.addEventListener('DOMContentLoaded', function() {
-    console.log("DOMcomplete");
-    dialog.showModal();
-    console.log("Dialog opened");
-});
-dialog.querySelector('.close').addEventListener('click', function() {
-    dialog.close();
-});
+
+function getToken(client_id, client_secret, code){
+    return fetch("https://slack.com/api/oauth.access?client_id="+ client_id "+&client_secret=" + client_secret + "&code=" + code).then(function(response) {
+        return JSON.parse(response.json()||"null");;
+    }).then(function(json) {
+        return json["access_token"];
+    });
+}
