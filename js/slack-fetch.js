4626fb0f871ec476d7a303477fb4c7314d304494
mecaota <mecaota@users.noreply.github.com>
2018-04-12 23:35:34 +0900

    add redirect_url parameter and encodeURI method

--- js/slack-fetch.js
+++ js/slack-fetch.js
@@ -1,3 +1,4 @@
+var url = "https://homo-geshuku.github.io/slack-carshare/index"
 var args = getURLparam(location.search.substring(1).split('&'));
 var cookies = getCookieObject(document.cookie);
 var token = "";
@@ -9,17 +10,18 @@ if("token" in cookies){
         "client_id" in args ? args["client_id"]
         : "client_id" in cookies ? cookies["client_id"]
         : "";
-    document.cookie = "client_id=" + client_id + "; max-age=604800";
+    document.cookie = "client_id=" + encodeURIComponent(client_id) + "; max-age=604800";
     var client_secret = "client_secret" in args ? args["client_secret"]
         : "client_secret" in cookies ? cookies["client_secret"]
         : "";
-    document.cookie = "client_secret=" + client_secret + "; max-age=604800";
+    document.cookie = "client_secret=" + encodeURIComponent(client_secret) + "; max-age=604800";
     var code = "code" in args ? args["code"]
         : "code" in cookies ? cookies["code"]
-        : getCode(client_id, "channels:history");
-    token = getToken(client_id, client_secret, code);
+        : getCode(client_id, "channels:history", url);
+
+    token = getToken(client_id, client_secret, code, url);
 }
-document.cookie = "token=" + token + "; max-age=604800";
+document.cookie = "token=" + encodeURIComponent(token) + "; max-age=604800";
 
 function getURLparam(pair){
     var arg = new Object;
@@ -39,11 +41,11 @@ function getCookieObject(cookie_str){
     return cookies;
 }
 
-function getCode(client_id, scope){
+function getCode(client_id, scope, url){
    location.href = "https://slack.com/oauth/authorize?client_id=" + client_id + "&scope=" + scope;
 }
 
-function getToken(client_id, client_secret, code){
+function getToken(client_id, client_secret, code, url){
     var token = "";
     fetch("https://slack.com/api/oauth.access?client_id=" + client_id + "&client_secret=" + client_secret + "&code=" + code).then(function(response) {
         return JSON.parse(response.json()||"null");
